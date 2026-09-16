407b0b6fd7e0592f0bb009c6dceecaee8f01445b
mayank0703 <mayankbodwal03@gmail.com>
2026-09-16 21:37:50 +0530

    first commit all classes are files

---
+++ 14sept/datat_types.py
@@ -0,0 +1,13 @@
+
+x=(9*9)
+y=int(x)
+print((y))
+
+xy="450"
+yx=int(xy)
+print((yx))
+
+print(bool(0))
+print(bool(5))
+print(bool(""))
+print(bool("Hello"))
---
+++ 15septt/string.py
@@ -0,0 +1,19 @@
+'''name="mayank"
+classs = '"btech"'
+rollno= """1234"""
+print("Name:", name)
+print("Class:", classs)         
+print("Roll No:", rollno)
+
+a= "python"
+print(a[0:3]) 
+print(a[1:4])
+print(a[2:5])
+print(a[3:6])
+print(a[0:3])
+print(a[-3:-1])
+print(a[-2])
+'''
+first = "data"
+second = " science"
+print(first + ""+ second)
