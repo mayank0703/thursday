5fb6f72c1b085de0dae35a750f9fbc801b462f25
mayank0703 <mayankbodwal03@gmail.com>
2026-09-18 09:18:18 +0530

    display_expense_records_test

---
+++ 16sept/session.py
@@ -0,0 +1,8 @@
+def display_expense_records(records):
+  for i in range(0,4):
+    category = 50
+    amount = 15
+    date = 11
+    d= category, amount, date
+    print(d)
+display_expense_records()
